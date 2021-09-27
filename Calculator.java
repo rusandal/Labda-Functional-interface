0d9fe3fc8d17017f13cf7caef8b1b9de6b13a510
sandal <ru-sandal@yandex.ru>
2021-09-27 14:41:53 +0300

    Lambda and functional interface v1

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> divide = (x, y) -> {
+        try {
+            if (y == 0) throw new ArithmeticException("Exception: Деление на 0");
+            return x / y;
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    };
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+    }
