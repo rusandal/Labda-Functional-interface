2206d9d914998ea13d59a6f4b8aef5cc45e74737
sandal <ru-sandal@yandex.ru>
2022-03-29 17:09:04 +0300

    Added Readme.md

--- Main.java
+++ Main.java
@@ -2,8 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1,1);
+        int b = calc.minus.apply(10,4);
         int c = calc.divide.apply(a, b);
+        System.out.println("a="+a+" b="+b+" a/b=");
         calc.println.accept(c);
+        int x = calc.pow.apply(a);
+        System.out.println("a*a="+x);
+        int y = calc.abs.apply(b);
+        System.out.println("b(+/-)="+y);
+        boolean count = calc.isPositive.test(b);
+        System.out.println("b>0? "+count);
     }
 }
